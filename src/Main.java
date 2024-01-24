a95b6a2cdec66aefef9b2151743e4157fc99c669
nyamarinette <nyamarinette2020@gmail.com>
2024-01-24 10:50:20 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+
+public class Main {
+    public static void main(String[] args) {
+        /*Nous avons ici un example d'égalite donc a la sortie si cette valeur est égale
+         nous aurons le résultat true qui veut dire qu'elle est vraie
+    boolean value = (24==24);
+        System.out.println(value);
+;
+        }
+    }
+    */
+
+ /* expréssion de la condition en utilisant le if qui veut dire( si ), ici cette instrustion est ignoree
+        paceque cette condition n est pas vraie donc elle est fausse (false ) puisque la valeur donnée est
+                14 et non 30.
+
+    int value = 14;
+    if (value ==30)
+        System.out.println("value= 30");
+    }
+            }
+            */
+        /* Ici j ai compile de facon a ne pas trouver une valeur d'ou j'ai mit else (si non)
+
+        int value = 7;
+        if (value == 30 || value == 6)
+            System.out.println("Valeur trouvée");
+        else
+            System.out.println("Aucune valeur trouvée");
+    }
+}
+/*
+         */
+        /*
+        int value = 7;
+        if (value < 5) {
+            System.out.println("Value < 5");
+        } else if (value > 90) {
+            System.out.println("value > 90");
+        } else {
+            System.out.println(value);//qui signifi que cette valeur n'est pas plus petit que 5 et plus grand que 90
+        }
+    }
+}
+/*
+
+         /*Ici je vais tester une variable avec un ( switch ) dans un cas */
+
+        int option = 200;
+        switch (option)
+        {
+            case 1:
+                /* if(option == 200) */
+                System.out.println("option= 200");
+                break;
+            default:
+
+                System.out.println("option != 200");
+                break;
+        }
+    }
+}
